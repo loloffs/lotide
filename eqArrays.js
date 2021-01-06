55c97a27878ec5f474136dd3e038950de0a05c8d
loloffs <lukeoloffs@gmail.com>
2021-01-06 02:36:14 +0000

    eqArray checks if 2 arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`✖Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const eqArray = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+   return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+};
+
+
+assertEqual(eqArray([1, 2, 3], [1, 2, 3]), true);
