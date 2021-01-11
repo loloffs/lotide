ee26dedeb87287eba1f74338cc294e732784c319
loloffs <lukeoloffs@gmail.com>
2021-01-11 23:40:28 +0000

    Implement object shorthand for index.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -40,15 +40,15 @@ const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(assertObjectsEqual(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(assertObjectsEqual(ab, abc)); // => false
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(assertObjectsEqual(ab, abc)); // => false
 
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 console.log(assertObjectsEqual(cd, dc)); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 6] };
-console.log(assertObjectsEqual(cd, cd2)); // => false
+// const cd2 = { c: "1", d: ["2", 3, 6] };
+// console.log(assertObjectsEqual(cd, cd2)); // => false
 
 module.exports = assertObjectsEqual;
